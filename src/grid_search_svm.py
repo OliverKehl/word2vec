d8b713eb2a1c205366a595aba6077cd3c37c6264
oliver kehl <kangjihua_home@126.com>
2015-07-11 17:21:18 +0800

    asd

--- src/grid_search_svm.py
+++ src/grid_search_svm.py
@@ -3,7 +3,8 @@ import numpy as np
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 import re
-
+import warnings
+warnings.filterwarnings('ignore')
 
 stop = set(stopwords.words('english'))
 vectorizer = None
@@ -17,24 +18,20 @@ def filter(review):
     
 
 def readFile(filename):
-    
     global vectorizer
     
     train_data = pd.read_csv(filename, header=0, delimiter='\t', quoting=3)
     train_size = train_data.shape[0]
-    
-    
     
     clean_train = []
     for i in xrange(0,train_size):
         clean_train.append(filter(train_data['review'][i]))
         if i%1000 ==0:
             print '%d reviews processed...' %i
-    from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+    from sklearn.feature_extraction.text import TfidfVectorizer
     
-    #vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None, max_features = 5000)
     if vectorizer==None:
-        vectorizer = TfidfVectorizer(sublinear_tf=True,max_df=0.5)
+        vectorizer = TfidfVectorizer(sublinear_tf=True,max_df=0.9,ngram_range=(1,3),max_features=100000)
         train_data_feature = vectorizer.fit_transform(clean_train)
     else:
         vec = TfidfVectorizer(vocabulary=vectorizer.vocabulary_)
@@ -46,7 +43,6 @@ def readFile(filename):
         return train_data['id'], train_data_feature
     else:
         return train_data['id'], train_data_feature, train_data['sentiment']
-    #test = pd.read_csv('',header=0,delimiter='\t', quoting=3)
 
 def forest_train(train_file):
     _,x,y = readFile(train_file)
@@ -61,28 +57,50 @@ def forest_predict(test_file,forest):
     
     output = pd.DataFrame( data={"id":id, "sentiment":y} )
     output.to_csv( "/Users/oliverkehl/Desktop/Bag_of_Words_model_tfidf.csv", index=False, quoting=3 )
-    
-def nn_train(train_file):
-    _,x,y = readFile(train_file)
-    import sklearn.neural_network as nn
-    model = nn.BernoulliRBM(n_components = 200, learning_rate = 0.1,batch_size = 2000, n_iter = 50, random_state = 1 , verbose=1)
-    model = model.fit(x,y)
-    return model
 
-def nn_predict(test_file,forest):
-    id,x = readFile(test_file)
-    y = forest.predict(x)
-    output = pd.DataFrame( data={"id":id, "sentiment":y} )
-    output.to_csv( "/Users/oliverkehl/Desktop/svm_result.csv", index=False, quoting=3 )
+def svm_train(train_file,test_file):
+    _,x,y = readFile(train_file)
+    id, tx = readFile(test_file)
+    #feature selection
+    from sklearn.feature_selection import SelectKBest,chi2
+    fselect = SelectKBest(chi2, k =5000)
+    x = fselect.fit_transform(x,y)
+    tx = fselect.transform(tx)
+
+    print x.shape
+    print tx.shape    
+
+    hehe = np.concatenate((x,tx))
+    from sklearn.preprocessing import scale
+    hehe = scale(hehe,with_mean=False)
+    x = hehe[0:x.shape[0]]
+    tx = hehe[x.shape[0]:]
     
+    from sklearn.cross_validation import train_test_split
+    tmp_array = np.arange(x.shape[0])
+    train_i,test_i = train_test_split(tmp_array, train_size = 0.8, random_state = 1024)
+    train_x = x[train_i]
+    train_y = y[train_i]
+    test_x = x[test_i]
+    test_y = y[test_i]
+
+    from sklearn.svm import SVC
+    model = SVC(probability=True)
+    model.fit(x,y)
+    
+    res1 = model.predict_proba(train_x)
+    res2 = model.predict_proba(test_x)
+    from sklearn.metrics import roc_auc_score
+    score1 = roc_auc_score(train_y, res1[:,1])
+    score2 = roc_auc_score(test_y, res2[:,1])
+    print score1
+    print score2
+
+    res = model.predict_proba(tx)
+    output = pd.DataFrame( data={"id":id, "sentiment":res[:,1]} )
+    output.to_csv( "/home/chuangxin/SVM_result.csv", index=False, quoting=3 )
+    
+    return model
+       
 if __name__=='__main__':
-    model = nn_train('/Users/oliverkehl/Downloads/labeledTrainData.tsv')
-    nn_predict('/Users/oliverkehl/Downloads/testData.tsv',model)
-    
-    
-    
-    
-    
-    
-    
-    
+    model = svm_train('/home/chuangxin/labeledTrainData.tsv','/home/chuangxin/testData.tsv')
--- src/hehe.py
+++ src/hehe.py
@@ -5,6 +5,8 @@ from nltk.corpus import stopwords
 import re
 from sklearn.linear_model import LogisticRegression as LR
 
+import warnings
+warnings.filterwarnings('ignore')
 
 stop = set(stopwords.words('english'))
 vectorizer = None
@@ -35,7 +37,7 @@ def readFile(filename):
     
     #vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None, max_features = 5000)
     if vectorizer==None:
-        vectorizer = TfidfVectorizer(sublinear_tf=True,ngram_range = ( 1, 3 ), max_features=40000)
+        vectorizer = TfidfVectorizer(sublinear_tf=True,ngram_range = ( 1, 3 ), max_features=100000, max_df=0.9)
         train_data_feature = vectorizer.fit_transform(clean_train)
     else:
         vec = TfidfVectorizer(vocabulary=vectorizer.vocabulary_)
@@ -49,44 +51,34 @@ def readFile(filename):
         return train_data['id'], train_data_feature, train_data['sentiment']
     #test = pd.read_csv('',header=0,delimiter='\t', quoting=3)
 
-def forest_train(train_file):
-    _,x,y = readFile(train_file)
-    from sklearn.ensemble import RandomForestClassifier
-    forest = RandomForestClassifier(n_estimators = 100, max_features = 'auto',  random_state = 50)
-    forest = forest.fit(x,y)
-    return forest
-
-def forest_predict(test_file,forest):
-    id,x = readFile(test_file)
-    y = forest.predict(x)
-    
-    output = pd.DataFrame( data={"id":id, "sentiment":y} )
-    output.to_csv( "/home/kehl/Desktop/random_forest.csv", index=False, quoting=3 )
-    
 def svm_train(train_file):
     _,x,y = readFile(train_file)
+    print 'reading done.'
     
     from sklearn.cross_validation import train_test_split
     tmp_array = np.arange(x.shape[0])
-    train_i, test_i = train_test_split(tmp_array, train_size = 0.8, random_state = 44)
+    train_i, test_i = train_test_split(tmp_array, train_size = 0.8, random_state = 500)
     
+    #from sklearn import preprocessing as pp
+    #scaler = pp.StandardScaler()
+    #x = scaler.fit(x)
+    #print 'scale done.'
+
     train_x = x[train_i]
     test_x = x[test_i]
     train_y = y[train_i]
     test_y = y[test_i]
-    #from sklearn.svm import SVC
-    #classifier = SVC(C=1.0,gamma = 0.0)
-    #classifier.fit(train_x,train_y)
-    
-    lr = LR()
-    lr.fit(train_x,train_y)
-    #res = lr.predict_proba(test_x)
-    res = lr.predict_proba(test_x)
+    from sklearn.svm import LinearSVC
+    classifier = LinearSVC()
+    classifier.fit(train_x,train_y)
+    print 'train done.'    
+
+    res = classifier.predict(test_x)
     print res.shape
     from sklearn.metrics import roc_auc_score
-    score = roc_auc_score(test_y,res[:,1])
+    score = roc_auc_score(test_y,res)
     print score
-    return lr
+    return classifier
     
     '''
     grid search
@@ -105,12 +97,72 @@ def svm_train(train_file):
     return grid.best_estimator_
     '''
 
+def adaboost_train(train_file,test_file):
+    _,x,y = readFile(train_file)
+    print 'reading done.'
+    ts = x.shape[0]
+    id,x2 = readFile(test_file)
+    
+    print x.shape
+    print x2.shape    
+
+    x = np.concatenate((x,x2))
+    print 'concatenate done.'
+    from sklearn.preprocessing import scale
+    x = scale(x,with_mean=False)
+    print 'scale done.'
+
+    x2 = x[ts:]
+    x=x[0:ts]
+
+    from sklearn.feature_selection import SelectKBest,chi2
+    x = SelectKBest(chi2,k=50000).fit_transform(x,y)    
+
+
+    from sklearn.cross_validation import train_test_split
+    tmp_array = np.arange(x.shape[0])
+    train_i, test_i = train_test_split(tmp_array, train_size = 0.8, random_state = 500)
+
+    train_x = x[train_i]
+    test_x = x[test_i]
+    train_y = y[train_i]
+    test_y = y[test_i]
+
+    from sklearn.ensemble import BaggingClassifier
+    bagging = BaggingClassifier(LR(penalty='l2',dual=True),n_estimators = 10,max_samples=0.6,max_features=0.6)
+    bagging.fit(train_x,train_y)
+    print 'train done.' 
+    res = bagging.predict(train_x)
+    print res
+    from sklearn.metrics import roc_auc_score
+    score = roc_auc_score(train_y,res)
+    
+    res = bagging.predict_proba(train_x)
+    print res
+    score = roc_auc_score(train_y,res[:,1])
+    print score
+    print '-----------------------------------------'
+    
+    print res[:,1]
+    res = bagging.predict_proba(test_x)
+    score = roc_auc_score(test_y,res[:,1])
+    print score
+
+    y=bagging.predict_proba(x2)
+    output = pd.DataFrame( data={"id":id, "sentiment":y[:,1]} )
+    output.to_csv( "/home/chuangxin/Bagging_result.csv", index=False, quoting=3 )
+
+    return bagging
+
 def svm_predict(test_file,model):
     id,x = readFile(test_file)
     y = model.predict_proba(x)
     output = pd.DataFrame( data={"id":id, "sentiment":y[:,1]} )
-    output.to_csv( "/home/chuangxin/LR_result.csv", index=False, quoting=3 )
+    output.to_csv( "/home/chuangxin/Bagging_result.csv", index=False, quoting=3 )
     
 if __name__=='__main__':
-    model = svm_train('/home/chuangxin/labeledTrainData.tsv')
-    svm_predict('/home/chuangxin/testData.tsv',model)
+    #print '-------------------------------------- svm model roc score'
+    #model = svm_train('/home/chuangxin/labeledTrainData.tsv')
+    #print '-------------------------------------- adaboostclassifier model roc score'
+    model = adaboost_train('/home/chuangxin/labeledTrainData.tsv','/home/chuangxin/testData.tsv')
+    #svm_predict('/home/chuangxin/testData.tsv',model)
